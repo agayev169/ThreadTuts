9042cf76d3ddcf8cf3fb6689d5693f992420fcf9
agayev169 <agayev169@gmail.com>
2018-10-06 23:40:06 +0400

    Modify code

--- src/Counter/Counter.java
+++ src/Counter/Counter.java
@@ -1,35 +1,34 @@
 package Counter;
 
 public class Counter extends Thread {
-    private String name;
-    private CounterPrinter cp;
+    private String name; // Name of the Thread
+    private CounterPrinter cp; // CounterPrinter to print values from 0 to n
+    private int maxCount; // The value of n
+    private static int count = 0; // Static counter to know the order of finish
 
 
-    public Counter(String name, CounterPrinter cp) {
+    public Counter(String name, CounterPrinter cp, int maxCount) { // Constructor
         this.name = name;
         this.cp = cp;
+        this.maxCount = maxCount;
     }
 
     @Override
     public void run() {
-        synchronized (cp) {
-            cp.count(name);
+        synchronized (cp) { // Synchronizing CounterPrinter to Count to maxCount without interruption (2nd problem)
+            cp.count(name, maxCount); // Count!
+            System.out.println(name + " finished #" + count); // Finish message
+            count++; // Incrementing counter for the next Counter
         }
     }
 }
 
-class CounterPrinter extends Thread {
-    private int maxCount;
-
-    CounterPrinter(int maxCount) {
-        this.maxCount = maxCount;
-    }
-
-    void count(String name) {
-        for (int i = 0; i < maxCount; i++) {
-            System.out.println("Counter name: " + name + ", " + i);
+class CounterPrinter extends Thread { // CounterPrinter to print values from 0 to n
+    void count(String name, int maxCount) {
+        for (int i = 0; i < maxCount; i++) { // Loop to print the values
+            System.out.println("Counter name: " + name + ", " + i); // Print The value
             try {
-                sleep((long) (Math.random() * 5000));
+                sleep((long) (Math.random() * 5000)); // Sleep some time between 0 and 5 seconds
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- src/Counter/TestCounter.java
+++ src/Counter/TestCounter.java
@@ -2,14 +2,14 @@ package Counter;
 
 public class TestCounter {
     public static void main(String[] args) {
-        CounterPrinter cp = new CounterPrinter(5);
-        Counter c1 = new Counter("TIC", cp);
-        Counter c2 = new Counter("TAC", cp);
-        c1.start();
-        c2.start();
+        CounterPrinter cp = new CounterPrinter(); // CounterPrinter to print values from 0 to n
+        Counter c1 = new Counter("TIC", cp, 5); // Counter1
+        Counter c2 = new Counter("TAC", cp, 5); // Counter2
+        c1.start(); // Starting Counter1
+        c2.start(); // Starting Counter2
         try {
-            c1.join();
-            c2.join();
+            c1.join(); // Joining Counter1
+            c2.join(); // Joining Counter2
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
